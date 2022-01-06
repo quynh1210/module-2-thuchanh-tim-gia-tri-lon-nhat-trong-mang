fa8fad09401d4e35afe84496234d7c03a8b75396
quynh1210 <quynh1210.nt@gmail.com>
2022-01-06 17:18:38 +0700

    add file

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,41 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner Scanner = new Scanner(System.in);
+        do {
+            System.out.print("Enter a size:");
+            size = Scanner.nextInt();
+            if (size > 20)
+                System.out.println("Size should not exceed 20");
+        } while (size > 20);
+
+        array = new int[size];
+        int i = 0;
+        while (i < array.length) {
+            System.out.print("Enter element" + (i + 1) + " : ");
+            array[i] = Scanner.nextInt();
+            i++;
+        }
+
+        System.out.print("Property list: ");
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + "\t");
+        }
+
+        int max = array[0];
+        int index = 1;
+        for (int j = 0; j < array.length; j++) {
+            if (array[j] > max) {
+                max = array[j];
+                index = j + 1;
+            }
+        }
+        System.out.println("The largest property value in the list is " + max + " ,at position " + index);
+    }
+}
